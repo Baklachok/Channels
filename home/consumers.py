df674675e6a11da72c35917f0ca4aedb31011516
Baklachok <korobko_01@inbox.ru>
2023-11-10 16:07:39 +0300

    addded notifications

--- home/consumers.py
+++ home/consumers.py
@@ -10,14 +10,21 @@ class TestConsumer(WebsocketConsumer):
         self.room_name = "test_consumer"
         self.room_group_name = "test_consumer_group"
         async_to_sync(self.channel_layer.group_add)(
-            self.room_name, self.room_group_name
+            self.room_group_name,
+            self.channel_name,
         )
         self.accept()
         self.send(text_data=json.dumps({'status': 'connected'}))
 
     def receive(self, text_data):
         print(text_data)
-        self.send(text_data=json.dumps(text_data))
+        self.send(text_data=json.dumps({'status': 'we got you'}))
 
     def disconnect(self, *args, **kwargs):
         print('disconnected')
+
+    def send_notification(self, event):
+        print('send_notification')
+        data = json.loads(event.get('value'))
+        self.send(text_data=json.dumps({'payload': data}))
+        print('send_notification')
